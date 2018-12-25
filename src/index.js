3f5cec56dcf7d61406b6f068a695ca1140a4945e
Min Lee <minho.lee.93@hotmail.com>
2018-12-25 11:41:43 -0500

    Update index.js to handle new apollo-ws

--- src/index.js
+++ src/index.js
@@ -7,6 +7,9 @@ import { ApolloClient } from 'apollo-client';
 import { createHttpLink } from 'apollo-link-http';
 import { InMemoryCache } from 'apollo-cache-inmemory';
 import { setContext } from 'apollo-link-context';
+import { split } from 'apollo-link';
+import { WebSocketLink } from 'apollo-link-ws';
+import { getMainDefinition } from 'apollo-utilities';
 
 import App from './App';
 import './index.css';
@@ -14,23 +17,43 @@ import * as serviceWorker from './serviceWorker';
 
 import { AUTH_TOKEN } from './constants';
 
+const authToken = localStorage.getItem(AUTH_TOKEN);
+
 const httpLink = createHttpLink({
   uri: 'http://localhost:4000'
 });
 
 const authLink = setContext((_, { headers }) => {
-  const token = localStorage.getItem(AUTH_TOKEN);
-
   return {
     headers: {
       ...headers,
-      authorization: token? `Bearer ${token}`: ''
+      authorization: authToken ? `Bearer ${authToken}`: ''
     }
   };
 });
 
+const wsLink = new WebSocketLink({
+  uri: 'ws://localhost:4000',
+  options: {
+    reconnect: true,
+    connectionParams: {
+      authToken
+    }
+  }
+});
+
+const link = split(
+  ({ query }) => {
+    const { kind, operation } = getMainDefinition(query);
+
+    return kind === 'OperationDefinition' && operation === 'subscription';
+  },
+  wsLink,
+  authLink.concat(httpLink)
+);
+
 const client = new ApolloClient({
-  link: authLink.concat(httpLink),
+  link,
   cache: new InMemoryCache()
 });
 
